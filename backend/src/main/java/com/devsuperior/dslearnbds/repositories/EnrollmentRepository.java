343bdcf7f646685891bb68e7b7581a59c982ab7b
thizeh <thiago02_08@hotmail.com>
2022-07-08 09:42:54 -0300

    Exceptions, validations, security

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/EnrollmentRepository.java
@@ -3,7 +3,8 @@ package com.devsuperior.dslearnbds.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearnbds.entities.Enrollment;
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
 
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/ResourceRepository.java
@@ -2,8 +2,8 @@ package com.devsuperior.dslearnbds.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dslearnbds.entities.Reply;
+import com.devsuperior.dslearnbds.entities.Resource;
 
-public interface ResourceRepository extends JpaRepository<Reply, Long> {
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
 }
